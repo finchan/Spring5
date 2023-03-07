084cd6902f23fcc9ed7a664da79ea4a77d508963
finchan <finchan@outlook.com>
2023-03-07 17:05:35 +0800

    Test

--- src/main/java/org/example/patterns/factory/factorymethod/DBConnectionPool.java
+++ src/main/java/org/example/patterns/factory/factorymethod/DBConnectionPool.java
@@ -1,13 +1,22 @@
 package org.example.patterns.factory.factorymethod;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Properties;
 import java.util.Vector;
+//https://github.com/gupaoedu-tom/spring5-samples
 
 public class DBConnectionPool extends Pool{
     //  正在使用的连接数
     private int checkedOut;
     //  存放产生的连接对象容器
     private Vector<Connection> freeConnections = new Vector<>();
+    private String url = null;
     private String password = null;
     private String username = null;
     //  空闲连接数
@@ -17,33 +26,145 @@ public class DBConnectionPool extends Pool{
     //  连接池实例变量
     private static DBConnectionPool pool = null;
 
+    public static synchronized DBConnectionPool getInstance() {
+        if(pool == null) {
+            pool = new DBConnectionPool();
+        }
+        return pool;
+    }
+
+    private DBConnectionPool() {
+        try{
+            init();
+            for(int i=0;i<normalConnect;i++) {
+                Connection c = newConnection();
+                if(c!=null){
+                    freeConnections.addElement(c);
+                    num++;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void init() throws IOException {
+        InputStream is = DBConnectionPool.class.getResourceAsStream(propertiesName);
+        Properties p = new Properties();
+        p.load(is);
+        this.username = p.getProperty("username");
+        this.password = p.getProperty("password");
+        this.driverName = p.getProperty("driverName");
+        this.url = p.getProperty("url");
+        this.maxConnect = Integer.parseInt(p.getProperty("maxConnect"));
+        this.normalConnect = Integer.parseInt(p.getProperty("normalConnect"));
+    }
+
+    @Override
+    public synchronized void freeConnection(Connection con) {
+        freeConnections.addElement(con);
+        num++;
+        checkedOut--;
+        numActive--;
+        notifyAll();
+    }
+
+    private Connection newConnection() {
+        Connection con = null;
+        try{
+            if(username == null) {
+                con = DriverManager.getConnection(url);
+            } else {
+                con = DriverManager.getConnection(url, username, password);
+            }
+            System.out.println("连接池创建一个新的连接");
+        } catch(SQLException e) {
+            System.out.println("无法创建这个URL的连接 " + url);
+        }
+        return con;
+    }
+
     @Override
     public void createPool() {
-
+        pool = new DBConnectionPool();
+        if(pool != null) {
+            System.out.println("创建连接池成功");
+        } else {
+            System.out.println("创建连接池失败");
+        }
     }
 
     @Override
-    public Connection getConnection() {
-        return null;
+    public synchronized Connection getConnection() {
+        Connection con = null;
+        if(freeConnections.size() > 0) {
+            num--;
+            con = (Connection) freeConnections.firstElement();
+            freeConnections.removeElementAt(0);
+            try {
+                if(con.isClosed()){
+                    System.out.println("从连接池中删除一个无效连接");
+                    con = getConnection();
+                }
+            } catch (SQLException e) {
+                System.out.println("从连接池中删除一个无效连接");
+                con = getConnection();
+            }
+        } else if(checkedOut < maxConnect) {
+            con = newConnection();
+        }
+        if(con != null) {
+            checkedOut++;
+        }
+        numActive++;
+        return con;
     }
 
     @Override
-    public Connection getConnection(long time) {
-        return null;
-    }
+    public synchronized Connection getConnection(long time) {
+        long startTime = new Date().getTime();
+        Connection con;
+        while((con = getConnection()) == null) {
+            try{
+                //线程等待
+                wait(time);
+            } catch (InterruptedException e) {
 
-    @Override
-    public void freeConnection(Connection conn) {
-
+            }
+            if((new Date().getTime() - startTime) >= time) {
+                return null;
+            }
+        }
+        return con;
     }
 
     @Override
     public int getNum() {
-        return 0;
+        return num;
     }
 
     @Override
     public int getNumActive() {
-        return 0;
+        return numActive;
+    }
+
+    @Override
+    public synchronized void release() {
+        try{
+            Enumeration<Connection> allConnections = freeConnections.elements();
+            while(allConnections.hasMoreElements()) {
+                Connection con = allConnections.nextElement();
+                try{
+                    con.close();
+                    num--;
+                } catch(SQLException e) {
+                    System.out.println("无法关闭连接池中的连接");
+                }
+            }
+            freeConnections.removeAllElements();
+            numActive = 0;
+        } finally {
+            super.release();
+        }
     }
 }
--- src/main/java/org/example/patterns/factory/factorymethod/Pool.java
+++ src/main/java/org/example/patterns/factory/factorymethod/Pool.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public abstract class Pool {
-    private String propertiesName = "connection-INF.properties";
+    public String propertiesName = "connection-INF.properties";
 
     private static Pool instance = null;
 
@@ -21,45 +21,6 @@ public abstract class Pool {
     protected String driverName = null;
     //  驱动变量
     protected Driver driver = null;
-
-    protected Pool() {
-        try {
-            init();
-            loadDriver(driverName);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private void init() throws IOException {
-        InputStream is = Pool.class.getResourceAsStream(propertiesName);
-        Properties p = new Properties();
-        p.load(is);
-        this.driverName = p.getProperty("driverName");
-        this.maxConnect = Integer.parseInt(p.getProperty("maxConnect"));
-        this.normalConnect = Integer.parseInt(p.getProperty("normalConnect"));
-    }
-
-    private void loadDriver(String dri) {
-        String driverClassName = dri;
-        try{
-            driver = (Driver) Class.forName(driverClassName).newInstance();
-            DriverManager.registerDriver(driver);
-            System.out.println("成功注册JDBC驱动程序 " + driverClassName);
-        } catch(Exception e) {
-            System.out.println("无法注册JDBC驱动程序 " + driverClassName + " error: " + e);
-        }
-    }
-
-    public abstract void createPool();
-
-    public static synchronized Pool getInstance() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        if(instance == null) {
-            instance = (Pool) Class.forName("org.example.patterns.factory.factorymethod.Pool").newInstance();
-        }
-        return instance;
-    }
 
     //  创建一个连接，没有则创建一个连接，且小于最大连接限制
     public abstract Connection getConnection();
@@ -75,6 +36,10 @@ public abstract class Pool {
     //  返回当前工作连接数
     public abstract int getNumActive();
 
+    public abstract void createPool();
+
+    public abstract void init() throws IOException ;
+
     protected synchronized void release() {
         try{
             DriverManager.deregisterDriver(driver);
@@ -83,4 +48,44 @@ public abstract class Pool {
             System.out.println("无法撤销JDBC驱动程序的注册：" + driver.getClass().getName());
         }
     }
+
+    /**
+     * 以下方法全部注释，相关功能标注为abstract方法
+     */
+//    protected Pool() {
+//        try {
+//            init();
+//            loadDriver(driverName);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
+
+//    private void init() throws IOException {
+//        InputStream is = Pool.class.getResourceAsStream(propertiesName);
+//        Properties p = new Properties();
+//        p.load(is);
+//        this.driverName = p.getProperty("driverName");
+//        this.maxConnect = Integer.parseInt(p.getProperty("maxConnect"));
+//        this.normalConnect = Integer.parseInt(p.getProperty("normalConnect"));
+//    }
+
+//    private void loadDriver(String dri) {
+//        try{
+//            driver = (Driver) Class.forName(dri).newInstance();
+//            DriverManager.registerDriver(driver);
+//            System.out.println("成功注册JDBC驱动程序 " + dri);
+//        } catch(Exception e) {
+//            System.out.println("无法注册JDBC驱动程序 " + dri + " error: " + e);
+//        }
+//    }
+
+//    public static synchronized Pool getInstance() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+//        if(instance == null) {
+//            instance = (Pool) Class.forName("org.example.patterns.factory.factorymethod.Pool").newInstance();
+//        }
+//        return instance;
+//    }
+
 }
